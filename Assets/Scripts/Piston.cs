d64baeab64d466b05020e7811c89fad5e2561f1f
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-09-22 17:57:39 -0400

    Fixed pistons
    Piston can be rotated and will work correctly

--- Assets/Scripts/Piston.cs
+++ Assets/Scripts/Piston.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 
 public class Piston : Toggleable
 {
-
     Vector3 movement;
-    Rigidbody pistonRigidbody;
-    public GameObject cylinder;
-    bool extended = false;
-    
+	public int extensionRange = 1;
+    public bool extended = false;
+    public Transform arm;
+    public Transform moveablePart;
     bool on;
+	bool isColliding = false;
 
     // Use this for initialization
     void Start () {
-        pistonRigidbody = GetComponent<Rigidbody>();
         on = false;
     }
 
     // Update is called once per frame
     void Update () {
-        
+        isColliding = false;
     }
 
     public override void TurnOn()
@@ -44,32 +43,32 @@ public class Piston : Toggleable
 
     public void Extend()
     {
-        transform.localPosition += new Vector3(-1f, 0, 0);
+        moveablePart.localPosition += new Vector3(-1f * extensionRange, 0, 0);
         //cylinder.transform.localPosition += new Vector3(-0.5f, 0, 0);
-        cylinder.transform.localScale += new Vector3(4f, 0, 0);
+        arm.localScale += new Vector3(4f * extensionRange, 0, 0);
         extended = true;
     }
 
     public void Contract()
     {
-        transform.localPosition -= new Vector3(-1f, 0, 0);
+        moveablePart.localPosition -= new Vector3(-1f * extensionRange, 0, 0);
         //cylinder.transform.localPosition += new Vector3(0.5f, 0, 0);
-        cylinder.transform.localScale += new Vector3(-4f, 0, 0);
+        arm.localScale += new Vector3(-4f * extensionRange, 0, 0);
         extended = false;
     }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (extended)
-        {
-            float newX = (float)Math.Round((pistonRigidbody.position.x - 1) * 2, MidpointRounding.AwayFromZero) / 2;
-            other.transform.localPosition = new Vector3(newX, other.transform.localPosition.y, other.transform.localPosition.z);
-        }
-    }
-
-
-    void OnTriggerExit(Collider other)
-    {
+	
+	void OnTriggerEnter(Collider other){
+		if (isColliding == true) return;
+		
+            Vector3 moveDirection = (other.transform.position - transform.position).normalized;
+			print(moveDirection);
+			other.transform.position += moveDirection * extensionRange;
+			isColliding = true;
         
-    }
+	}
+	
+	void OnTriggerExit(Collider other) {
+		;
+	}
+	
 }
