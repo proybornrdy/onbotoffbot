a3a6ab216998fd12af1d0880a07d3f48a76cac97
JeyWhy <JeyWhy@users.noreply.github.com>
2018-09-22 12:52:31 -0400

    Add files via upload
    Adding scripts folder w/ the abstract class Toggleable

---
+++ Assets/Scripts/Piston.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : Toggleable
+{
+
+    Vector3 movement;
+    Rigidbody pistonRigidbody;
+    bool on;
+
+    // Use this for initialization
+    void Start () {
+        pistonRigidbody = GetComponent<Rigidbody>();
+        on = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public override void TurnOn()
+    {
+        Extend();
+    }
+
+    public override void TurnOff()
+    {
+        Contract();
+    }
+
+    public void Extend()
+    {
+        on = true;
+        movement.Set(-1f, 0f, 0f);
+        pistonRigidbody.MovePosition(transform.position + movement);
+    }
+
+    public void Contract()
+    {
+        on = false;
+        movement.Set(1f, 0f, 0f);
+        pistonRigidbody.MovePosition(transform.position + movement);
+    }
+}
---
+++ Assets/Scripts/Toggleable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Toggleable : MonoBehaviour {
+
+    public abstract void TurnOn();
+
+    public abstract void TurnOff();
+}
