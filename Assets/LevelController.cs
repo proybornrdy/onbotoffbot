3bc754c708db2e1348afc7cc6b4c60fc6fb8ea30
mshulman2015 <mshulman2015@gamil.com>
2018-09-22 12:02:52 -0400

    basic UI

---
+++ Assets/LevelController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+	// Players
+	static GameObject OnPlayer;
+	static GameObject OffPlayer;
+
+	// Interactable Objects
+	static GameObject Door;
+	static GameObject[] Pistons;
+
+	// Game State
+	static private float time; // true: game still going, falst: game over
+	static private bool gamePlaying; // true: game still going, falst: game over
+	static private string reason; // reason game is over if it's over
+
+	public static bool getGameStatus()
+	{
+		return gamePlaying;
+	}
+
+	public static void endGame(string reason)
+	{
+		gamePlaying = false;
+		NewBehaviourScript.reason = reason;
+	}
+
+	public static float getTimePlayed()
+	{
+		return time;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (gamePlaying)
+		{
+			time += Time.deltaTime;
+		}
+	}
+}
