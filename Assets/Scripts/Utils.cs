0fe09f24fe2be2a8a67f04bae518b3a58b480baf
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-09-22 19:55:50 -0400

    Fixed weird player collisions for piston
    -Made trigger collider slightly smaller than the physical collider so that non-kinematic objects don't get bounced unnecessarily
    -Piston pushing is now snapped to the correct axis so that the player can't be pushed in a weird direction

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public static class Utils {
+
+	/**
+	Takes vector v and returns the nearest cardinal vector.  For ties, precedence is i > j > k
+	*/
+	public static Vector3 NearestCardinal(Vector3 v){
+		return Math.Abs(v.x) >= Math.Abs(v.y) && Math.Abs(v.x) >= Math.Abs(v.z) ?
+					new Vector3(v.x / Math.Abs(v.x), 0, 0) :
+				Math.Abs(v.y) >= Math.Abs(v.x) && Math.Abs(v.y) >= Math.Abs(v.z) ?
+					new Vector3(0, v.y / Math.Abs(v.y), 0) :
+					new Vector3(0, 0, v.z / Math.Abs(v.z));
+	}
+}
