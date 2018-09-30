2671a465c0c9f5ce9b3cd91aba14497629149b42
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-09-30 12:34:38 -0400

    Added multiple tag system

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Tags : MonoBehaviour {
+
+    public List<string> tags;
+
+    public bool HasTag(string s)
+    {
+        return tags.Any(t => t == s);
+    }
+}
