f96fc8cbb21028814d16b11bf03474a2f2dd3b7a
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-09-30 11:41:33 -0400

    Sorted scripts into folders

---
+++ Assets/Scripts/ObjectSpecific/OnButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnButton : MonoBehaviour {
+
+	public GameObject onPlayer;
+    public Toggleable toggleable;
+
+	// Use this for initialization
+	void Start () {
+        ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!LevelController.gameGoing())
+		{
+			return;
+		}
+    }
+    
+
+    void TurnOn()
+    {
+        Vector3 onPlayerPos = onPlayer.transform.position;
+        Vector3 buttonPos = transform.position;
+        if (Utils.InRange(onPlayerPos, buttonPos))
+        {
+            toggleable.TurnOn();
+        }
+    }
+
+    void OnEnable()
+    {
+        EventManager.OnOnPlayerInteracted += TurnOn;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnOnPlayerInteracted -= TurnOn;
+    }
+}
