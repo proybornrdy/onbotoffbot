2619e0791667e0e104cc6bd01865b6e31a58d644
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-09-30 11:42:17 -0400

    Simplified NearestCardinal

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -7,12 +7,21 @@ public static class Utils {
 	Takes vector v and returns the nearest cardinal vector.  For ties, precedence is i > j > k
 	*/
 	public static Vector3 NearestCardinal(Vector3 v){
-		return Math.Abs(v.x) >= Math.Abs(v.y) && Math.Abs(v.x) >= Math.Abs(v.z) ?
-					new Vector3(v.x / Math.Abs(v.x), 0, 0) :
-				Math.Abs(v.y) >= Math.Abs(v.x) && Math.Abs(v.y) >= Math.Abs(v.z) ?
-					new Vector3(0, v.y / Math.Abs(v.y), 0) :
-					new Vector3(0, 0, v.z / Math.Abs(v.z));
+        int largest = (int)LargestComponent(v);
+        Vector3 cardinal = Vector3.zero;
+        cardinal[largest] = Mathf.Sign(v[largest]);
+        return cardinal;
 	}
+
+    public enum Coordinate { x, y, z }
+
+    public static Coordinate LargestComponent(Vector3 v)
+    {
+        return
+            Math.Abs(v.x) >= Math.Abs(v.y) && Math.Abs(v.x) >= Math.Abs(v.z) ? Coordinate.x :
+            Math.Abs(v.y) >= Math.Abs(v.x) && Math.Abs(v.y) >= Math.Abs(v.z) ? Coordinate.y :
+            Coordinate.z;
+    }
 
     public static bool InRange(Vector3 onPlayerPos, Vector3 buttonPos)
     {
