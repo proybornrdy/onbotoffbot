5cd337b21c41854e2e3b1ecfbd0d16b53a36cb20
proybornrdy <seungq92@gmail.com>
2018-10-01 15:39:46 -0400

    attached new script to player to seethrough walls

---
+++ Assets/Scripts/Global Controllers/PlayerRaycast.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour {
+
+    public float rayDistance = 25;
+    public Vector3 camPosition;
+    private Color color;
+
+    private List<GameObject> invisibleObjs = new List<GameObject>();
+
+    private void Start()
+    {
+        camPosition = GameObject.FindWithTag("MainCamera").transform.position;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector3 rayDirection = camPosition - transform.position;
+        Ray ray = new Ray(transform.position, rayDirection);
+        RaycastHit[] hits= Physics.RaycastAll(ray, rayDistance);
+        Debug.DrawLine(transform.position, transform.position + rayDirection * rayDistance, Color.red);
+
+        List<GameObject> currentInvisibleObjects = new List<GameObject>();
+
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.DrawLine(hit.point, hit.point + Vector3.up * 5, Color.green);
+
+            GameObject collidedObj = hit.collider.gameObject;
+            color = collidedObj.GetComponent<Renderer>().material.color;
+            color.a = 0.5f;
+            collidedObj.GetComponent<Renderer>().material.color = color;
+
+
+            if (!currentInvisibleObjects.Contains(collidedObj))
+            {
+                currentInvisibleObjects.Add(collidedObj);
+            }
+        }
+        foreach (GameObject invisibleObj in invisibleObjs){
+            if (!currentInvisibleObjects.Contains(invisibleObj))
+            {
+                color = invisibleObj.GetComponent<Renderer>().material.color;
+                color.a = 1.0f;
+                invisibleObj.GetComponent<Renderer>().material.color = color;
+            }
+        }
+        invisibleObjs = currentInvisibleObjects;
+    }
+}
