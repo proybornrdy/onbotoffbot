11f9c9823c7838a753b70172a9d53cd0afc0764c
proybornrdy <seungq92@gmail.com>
2018-10-12 21:28:08 -0400

    added camera movement script, applied camera movement to levelcontrol script

---
+++ Assets/Scripts/Global Controllers/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 newPos;
+    public Vector3 currentPos;
+    public GameObject mainCamera;
+
+    private Vector3 cameraPos = new Vector3(-7.0f, 8.5f, -7.0f); //fixed offset of the camera from the center of the level
+
+    //public GameObject[] rooms;
+
+    public void changeCameraPos(GameObject room)
+    {
+        currentPos = mainCamera.transform.position;
+
+        Renderer[] rends = room.GetComponentsInChildren<Renderer>();
+        if(rends.Length == 0)//error checking just in case if room element is empty gameobject
+        {
+            newPos = room.transform.position;
+        }
+
+        Bounds parent = rends[0].bounds;
+        foreach(Renderer r in rends) //find center of the lelel and find suitable camera position from there
+        {
+            parent.Encapsulate(r.bounds);
+        }
+
+        newPos = parent.center + cameraPos;  
+        mainCamera.transform.position = Vector3.Lerp(currentPos, newPos, Time.deltaTime);
+    }
+
+}
