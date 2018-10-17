0c1d1f0c5a66044b25f22bbe61603d39c07bb5e5
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-10-17 15:53:29 -0400

    Tutorial text, updated character models

--- Assets/Scripts/ObjectSpecific/RoomCollider.cs
+++ Assets/Scripts/ObjectSpecific/RoomCollider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomCollider : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class RoomCollider : MonoBehaviour
     int playerOffEnters = 0;
     LevelController lc;
     Door door;
+    TextTrigger text;
 
     void Awake()
     {
         lc = GameObject.Find("LevelController").GetComponent<LevelController>();
         var parent = transform.parent.gameObject;
         door = parent.GetComponentInChildren<Door>();
+        text = GetComponentInChildren<TextTrigger>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,7 +25,11 @@ public class RoomCollider : MonoBehaviour
             playerOnEnters++;
         else if (other.gameObject.HasTag(Tag.PlayerOff))
             playerOffEnters++;
-        if (playerOnEnters > 0 && playerOffEnters > 0) lc.PlayersMovedToRoom(door.index);
+        if (playerOnEnters > 0 && playerOffEnters > 0)
+        {
+            lc.PlayersMovedToRoom(door.index);
+            if (text) text.TurnOn();
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -34,5 +41,9 @@ public class RoomCollider : MonoBehaviour
 
         if (playerOnEnters < 0) playerOnEnters = 0;
         if (playerOffEnters < 0) playerOffEnters = 0;
+        if (playerOnEnters > 0 && playerOffEnters > 0)
+        {
+            if (text) text.TurnOff();
+        }
     }
 }
---
+++ Assets/Scripts/ObjectSpecific/TextTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour {
+
+    public GameObject panel;
+    public GameObject text;
+
+	public void TurnOn()
+    {
+        panel.SetActive(true);
+        text.SetActive(true);
+    }
+
+    public void TurnOff()
+    {
+        panel.SetActive(false);
+        text.SetActive(false);
+    }
+}
