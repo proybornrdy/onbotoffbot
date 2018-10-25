4441913b2938a61f9f619412676b3d7185263302
proybornrdy <seungq92@gmail.com>
2018-10-25 15:42:41 -0400

    typo in camermovement variable

--- Assets/Scripts/Global Controllers/LevelController.cs
+++ Assets/Scripts/Global Controllers/LevelController.cs
@@ -95,7 +95,7 @@ public class LevelController : MonoBehaviour
 		if (gamePlaying)
 		{
 			time += Time.deltaTime;
-            if (!isTestLevel && rooms.Length !=0) cc.changeCameraPos(rooms[currentLevel][0]);
+            if (!isTestLevel && rooms.Length !=0) cc.changeCameraPos(rooms[currentRoom][0]);
 
 			// log postion every second
 			if (((int)time) != oldTime)
