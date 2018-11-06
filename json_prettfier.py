0d80f941e58de7a5bbf21a3f9502f2c1c2fb5133
mshulman2015 <mshulman2015@gamil.com>
2018-11-05 19:15:18 -0500

    json prettyfier

---
+++ json_prettfier.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import json
+
+for i in range(1, len(sys.argv)):
+    file_path =  sys.argv[i]
+    print(file_path)
+
+    with open(file_path) as f:
+        contents = json.load(f)
+
+    file_split = file_path.split('.')
+    out_file_path = '.'.join(file_split[:-1] + ["pretty"] + [file_split[-1]])
+    with open(out_file_path, 'w') as f:
+        json.dump(contents, f, sort_keys=True, indent=4)
+
+print("done")
