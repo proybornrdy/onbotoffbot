f2cb1e135c8a6be113aa0f950bfd6f9d754b3b52
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-11-20 19:22:09 -0500

    Tweaked layout

--- Assets/Scripts/Global Controllers/GameStateMachine.cs
+++ Assets/Scripts/Global Controllers/GameStateMachine.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateMachine : MonoBehaviour {
+public class GameStateMachine {
+    private static GameStateMachine instance = null;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static GameStateMachine Instance
+    {
+        get
+        {
+            if (instance == null) instance = new GameStateMachine();
+            return instance;
+        }
+    }
+
+    public bool CompletedRoom_11 { get; set; }
+
+    public bool CompletedRoom_12 { get; set; }
+
+    public bool CompletedRoom_13 { get; set; }
+
+    public bool CompletedRoom_14 { get; set; }
+
+    public bool CompletedRoom_15 { get; set; }
+
+    public bool CompletedRoom_16 { get; set; }
+
+    public bool CompletedRoom_21 { get; set; }
+
+    public bool CompletedRoom_22 { get; set; }
+
+    public GameObject CurrentRoom { get; set; }
+
+    public Vector3 OnPlayerPos { get; set; }
+
+    public Vector3 OffPlayerPos { get; set; }
+
+    private GameStateMachine()
+    {
+        ;
+    }
+
 }
