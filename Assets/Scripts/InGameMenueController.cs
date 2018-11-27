c0535f053e7007a10f9d55e750b9fefe17fd7d53
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-11-27 13:08:04 -0500

    menu changes

--- Assets/Scripts/InGameMenueController.cs
+++ Assets/Scripts/InGameMenueController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameMenueController : MonoBehaviour {
 
@@ -12,33 +13,49 @@ public class InGameMenueController : MonoBehaviour {
 	public GameObject left_pointer;
 	public GameObject right_pointer;
 
-	// Update is called once per frame
-	void Update () {
+    public Button btnToMenu;
+    public Button reset;
+
+    private void Start()
+    {
+        btnToMenu.onClick.AddListener(delegate () { SceneManager.LoadScene("MainMenu"); });
+        reset.onClick.AddListener(delegate () {
+            LevelController.ResetScene();
+            LevelController.InMenue = false;
+            });
+        }
+
+    // Update is called once per frame
+        void Update () {
 		if (Input.GetAxisRaw(PlayerInputTranslator.GetVerticalAxis(Player.ON)) > .5||
 			Input.GetAxisRaw(PlayerInputTranslator.GetVerticalAxis(Player.OFF)) > .5)
-		{
-			left_pointer.transform.localPosition = new Vector3(top_left.x, top_left.y, left_pointer.transform.localPosition.z);
-			right_pointer.transform.localPosition = new Vector3(top_right.x, top_right.y, left_pointer.transform.localPosition.z); ;
-		}
+        {
+            reset.Select();
+            //left_pointer.transform.localPosition = new Vector3(top_left.x, top_left.y, left_pointer.transform.localPosition.z);
+            //right_pointer.transform.localPosition = new Vector3(top_right.x, top_right.y, left_pointer.transform.localPosition.z); ;
+        }
 		if (Input.GetAxisRaw(PlayerInputTranslator.GetVerticalAxis(Player.ON)) < -.5 ||
 			Input.GetAxisRaw(PlayerInputTranslator.GetVerticalAxis(Player.OFF)) < -.5)
-		{
-			left_pointer.transform.localPosition = new Vector3(bottom_left.x, bottom_left.y, left_pointer.transform.localPosition.z);
-			right_pointer.transform.localPosition = new Vector3(bottom_right.x, bottom_right.y, left_pointer.transform.localPosition.z);
+        {
+            btnToMenu.Select();
+            //left_pointer.transform.localPosition = new Vector3(bottom_left.x, bottom_left.y, left_pointer.transform.localPosition.z);
+			//right_pointer.transform.localPosition = new Vector3(bottom_right.x, bottom_right.y, left_pointer.transform.localPosition.z);
 		}
 
-		if (Input.GetButton(PlayerInputTranslator.GetJump(Player.ON)) ||
-			Input.GetButton(PlayerInputTranslator.GetJump(Player.OFF)))
-		{
-			if (left_pointer.transform.localPosition.y == top_left.y)
-			{
-				SceneManager.LoadScene("MainMenue");
-			}
-			else
-			{
-				LevelController.ResetScene();
-				LevelController.InMenue = false;
-			}
-		}
+
+
+		//if (Input.GetButton(PlayerInputTranslator.GetJump(Player.ON)) ||
+		//	Input.GetButton(PlayerInputTranslator.GetJump(Player.OFF)))
+		//{
+		//	if (left_pointer.transform.localPosition.y == top_left.y)
+		//	{
+		//		SceneManager.LoadScene("MainMenue");
+		//	}
+		//	else
+		//	{
+		//		LevelController.ResetScene();
+		//		LevelController.InMenue = false;
+		//	}
+		//}
 	}
 }
