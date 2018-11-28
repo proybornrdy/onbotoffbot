dc5b6fa38537db5c4249368aa4e55455ba1e2f15
proybornrdy <proybornrdy@gmail.com>
2018-11-27 20:32:59 -0500

    modified camera so it consider multiple collider boxes

--- Assets/Scripts/Global Controllers/CameraController.cs
+++ Assets/Scripts/Global Controllers/CameraController.cs
@@ -2,38 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public Vector3 newPos;
     public Vector3 currentPos;
 
-    private Vector3 cameraPos; 
+    private Vector3 cameraPos;
 
     //public GameObject[] rooms;
+    private bool onlyonce = false;
 
     public void changeCameraPos(Room room)
     {
         currentPos = Camera.main.transform.position;
-        
-        Vector3 roomSize = room.GetComponent<Collider>().bounds.size;
-        Bounds roomBound = room.GetComponent<Collider>().bounds;
-        Vector3 roomCenter = roomBound.center;
-        roomCenter.x = roomCenter.x -roomSize.x *0.25f;
-        roomCenter.z = roomCenter.z - roomSize.z * 0.25f;
+        Vector3 roomSize = new Vector3(0, 0, 0);
+        Vector3 roomCenter = new Vector3(0, 0, 0);
+        float[] lengths = new float[3 * room.GetComponents(typeof(Collider)).Length];
 
-        float[] lengths = new float[3];
-        lengths[0] = roomSize.x;
-        lengths[1] = roomSize.y;
-        lengths[2] = roomSize.z;
-        
+        int i = 0;
+        foreach (Collider col in room.GetComponents(typeof(Collider)))
+        {
+            Vector3 colSize = col.bounds.size;
+            Bounds colBound = col.bounds;
+            Vector3 colCenter = colBound.center;
+            colCenter.x = colCenter.x - colSize.x * 0.25f;
+            colCenter.z = colCenter.z - colSize.z * 0.25f;
+            roomCenter.x += colCenter.x;
+            roomCenter.z += colCenter.z;
+            roomCenter.y += colCenter.y;
+
+            Debug.Log(colSize + " " + i);
+
+            lengths[0 + i * 3] = colSize.x;
+            lengths[1 + i * 3] = colSize.y;
+            lengths[2 + i * 3] = colSize.z;
+            i++;
+            Debug.Log("length/?? " + lengths[0] + " " + lengths[1] + " " + lengths[2]);
+
+        }
+        roomCenter.x = roomCenter.x / room.GetComponents(typeof(Collider)).Length;
+        roomCenter.y = roomCenter.y / room.GetComponents(typeof(Collider)).Length;
+        roomCenter.z = roomCenter.z / room.GetComponents(typeof(Collider)).Length;
+
+
+
+
+
+
+
         float longSide = Mathf.Max(lengths);
+        Debug.Log("longside check " + lengths[0] + " " + lengths[1] + " " + lengths[2]);
 
-        float yAxis = Mathf.Tan(Camera.main.transform.eulerAngles.x) * Mathf.Sqrt(Mathf.Pow(longSide,2f)*2);
+        float yAxis = Mathf.Tan(Camera.main.transform.eulerAngles.x) * Mathf.Sqrt(Mathf.Pow(longSide, 2f) * 2);
 
-        cameraPos = new Vector3(roomCenter.x - longSide, roomCenter.y+yAxis, roomCenter.z - longSide) ;
+        cameraPos = new Vector3(roomCenter.x - longSide, roomCenter.y + yAxis, roomCenter.z - longSide);
 
         //this below line deals how much screen is zoomed. Bigger the multiplier, lesser the screen zoomed.
-        Camera.main.orthographicSize = longSide*0.8f;
+        Camera.main.orthographicSize = longSide * 0.8f;
 
         //Debug.Log("~~~~~"+ room.name);
         //Debug.Log(roomSize);
